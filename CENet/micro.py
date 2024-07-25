fdf4e2a479a6d9625ba1b3c7609b8f871913354c
XYQ <1932772459@qq.com>
2024-07-25 21:02:37 +0800

    1.update train.py 2.add backbone 3.add micro.py 4.update the structure of folder

--- CENet/micro.py
+++
@@ -1,8 +0,0 @@
-
-TRAIN=1 #train datasets
-VAL=2   #val datasets
-TEST=3  #test datasets
-
-CNN='CNN'                   #VGG
-MAMBA='Mamba'               #VMUNet
-TRANSFORMER='Transformer'   #Transformer
